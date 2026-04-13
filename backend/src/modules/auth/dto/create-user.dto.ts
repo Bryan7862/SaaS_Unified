79b052ed2d3acfbc06390748383508b7c6032a30
Sieghart Saladin <hisokamorow786@gmail.com>
2026-04-13 16:55:11 -0500

    chore(auth):Se rebajo la dureza de contraseña a minimo 6 caracteres sin restricciones de simbolos

--- backend/src/modules/auth/dto/create-user.dto.ts
+++ backend/src/modules/auth/dto/create-user.dto.ts
@@ -14,10 +14,7 @@ export class CreateUserDto {
     email: string;
 
     @IsNotEmpty()
-    @MinLength(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
-    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/, {
-        message: 'La contraseña debe incluir mayúscula, minúscula, número y carácter especial (@$!%*?&)',
-    })
+    @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
     password: string;
 
     @IsOptional()
